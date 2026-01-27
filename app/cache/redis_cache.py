b5aee8b41d4071f8c68f4e85eb884800055908e6
semaphore <beherapranaya071@gmail.com>
2026-01-27 20:23:45 +0530

    added render.py

--- app/cache/redis_cache.py
+++ app/cache/redis_cache.py
@@ -1,3 +1,29 @@
+import os
+import redis
+from dotenv import load_dotenv
+
+load_dotenv()
+
+REDIS_URL = os.getenv("REDIS_URL")
+
+redis_client = redis.StrictRedis.from_url(REDIS_URL, decode_responses=True)
+
+
+def get_cached_prediction(key: str):
+    value = redis_client.get(key)
+    return eval(value) if value else None
+
+
+def set_cached_prediction(key: str, value: dict):
+    redis_client.set(key, str(value))
+
+
+
+
+
+
+
+"""
 import json 
 import redis
 from app.core.config import settings
@@ -15,7 +41,7 @@ def set_cached_prediction(key: str, value: dict, expiry: int = 3600):
     redis_client.setex(key, expiry, json.dumps(value))
 
 
-"""
+
 If Redis fails/breaks, continue normlly without cache
 
 def get_cached_prediction(key: str):
