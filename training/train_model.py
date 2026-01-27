52ff60ce973c23eb05fa2370e1a4954c218c0688
semaphore <beherapranaya071@gmail.com>
2026-01-27 21:01:34 +0530

    try to fix -  np is not defined error

--- training/train_model.py
+++ training/train_model.py
@@ -1,7 +1,6 @@
 import os
 import joblib
 import pandas as pd
-import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
