54b51e0028dd350db67dd0184c1a61fc83854c74
semaphore <beherapranaya071@gmail.com>
2026-01-27 18:26:29 +0530

    update train_model.py

--- training/train_model.py
+++ training/train_model.py
@@ -2,8 +2,6 @@ import os
 import joblib
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
